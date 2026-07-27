e355acec73cad6fb32b81fd4cfc8d4333059bf12
hanbee jeong <hb.jade00@gmail.com>
2026-07-27 15:10:36 +0900

    initial commit

---
+++ src/main/java/com/bibi/jangokeeper/JangokeeperApplication.java
@@ -0,0 +1,13 @@
+package com.bibi.jangokeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JangokeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JangokeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bibi/jangokeeper/JangokeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibi.jangokeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JangokeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
